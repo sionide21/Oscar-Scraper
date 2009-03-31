8fccf8b9de580bfd94ffb6e1a222599160fbc56a
Ben Olive <ben.olive@gatech.edu>
2009-03-31 11:37:54 -0400

    Changed output type of javascript.php to application/javascript

--- javascript.php
+++ javascript.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("jsmin-1.1.1.php");
+	header("Content-Type: application/javascript");
 	$outputFile = './outputModules/' . $_GET['outputModule'] . 'Output.js';
 	if (file_exists($outputFile)) {
 	print JSMin::minify(file_get_contents($outputFile) . file_get_contents('./OscarCrawler.js'));

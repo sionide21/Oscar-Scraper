0379d28e7f81350a9a7c6a49ac6f55ec4f8028c8
Ben Olive <ben.olive@gatech.edu>
2009-03-30 23:22:09 -0400

    Added web interface

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Oscar Data Collection</title>
+		<link rel="stylesheet" href="style.css" />
+		<script>
+			function jsUrl(om) {
+				return "javascript:(function(){var sc=document.createElement('script');sc.src='http://<?= $_SERVER['HTTP_HOST']?>/oscar/javascript.php?outputModule="
+				+ om
+				+ "';document.body.appendChild(sc);})();";
+			}
+			function updateOM(node) {
+				if (node.value == '') {
+					document.getElementById('urlBar').innerHTML = "Select an output format to get a url."
+				} else {
+					document.getElementById('urlBar').innerHTML = jsUrl(node.value);
+				}
+			}
+		</script>
+	</head>
+	<body onload="updateOM(document.getElementById('changebar'))">
+		<p>This program is very simple to use. Start by searching for the classes you want to scrape in oscar in a separate window. Next, select the format you want the data to be in. When you choose a format, the green box will display a url. Copy the entire url (including "javascript:") into the address bar for the oscar page. When you press enter, you should see the data in the format you selected. 
+		<form action="#">
+			<select name="outputModule" onChange="updateOM(this)" id="changebar">
+				<option value="">---Select an Output Format---</option>
+				<option value="JSON">JSON</option>
+			</select>
+		</form>
+		<div id="urlBar">Select an output format to get a url.</div>
+	</body>
+</html>
---
+++ javascript.php
@@ -0,0 +1,9 @@
+<?php
+	require_once("jsmin-1.1.1.php");
+	$outputFile = './' . $_GET['outputModule'] . 'Output.js';
+	if (file_exists($outputFile)) {
+	print JSMin::minify(file_get_contents($outputFile) . file_get_contents('./OscarCrawler.js'));
+	} else {
+		die("alert('Could not find output module');");
+	}
+?>
